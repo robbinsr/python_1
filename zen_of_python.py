101ce375d2cebd13c436e9193152bc4a16260e83
Russ Robbins <russ.robbins@outlook.com>
2016-08-28 13:04:28 -0400

    set the tone with this

---
+++ zen_of_python.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+_interpreters_ = ['Python 3.5.2 (CPython)']
+__author__ = "Russ Robbins"
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "Russ Robbins"
+__email__ = "russ.robbins@outlook.com"
+__encoding__ = "UTF-8"
+__acknowledgements = ['https://hg.python.org/peps/file/tip/pep-0020.txt']
+
+import this
