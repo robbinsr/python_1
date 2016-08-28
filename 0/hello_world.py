9596ddb81f480bb22ee0fa95622fa3e7e3bee30a
Russ Robbins <russ.robbins@outlook.com>
2016-08-28 13:19:42 -0400

    hello world example

---
+++ 0/hello_world.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+_interpreters_ = ['Python 3.5.2 (CPython)']
+__author__ = "Russ Robbins"
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "Russ Robbins"
+__email__ = "russ.robbins@outlook.com"
+__encoding__ = "UTF-8"
+__acknowledgements = ['https://hg.python.org/peps/file/tip/pep-0020.txt']
+
+print("Hello, World!")
+
+# run at os command prompt with python hello_world.py in the same directory
+# as the file
+
+# run at python repl with import hello_world
